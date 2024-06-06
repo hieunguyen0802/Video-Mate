50f0d8862e598c35aabac79d7cbbd227232482d5
hieunguyen0802 <hieu.h.t.nguyen@tcu.edu>
2024-06-06 18:33:23 +0700

    working on index screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, ScrollView, View, Image} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from "expo-router";
+
+import { images } from "../constants";
 
 export default function App() {
+  useEffect(() => {
+    console.log( typeof images.logo);
+  }, []);
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pbold">App fdsafanè!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{color:"blue"}}>Go to Profile</Link>
-    </View>
+    <SafeAreaView  className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+        
+            <Image source={images.logo}
+            width={100}
+             height={100} 
+             resizeMode="contain" />
+            <Text className="text-white text-5xl" >{images.logo + 1}</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
